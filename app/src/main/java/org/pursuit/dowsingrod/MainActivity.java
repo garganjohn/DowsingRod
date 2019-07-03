5f3df27bc2384acf3255a6b32daa978672908a2c
garganjohn <garganjohn@gmail.com>
2019-07-03 19:05:52 -0400

    added base GoogleMaps dependency, and migrated to androidx

--- app/src/main/java/org/pursuit/dowsingrod/MainActivity.java
+++ app/src/main/java/org/pursuit/dowsingrod/MainActivity.java
@@ -1,13 +1,26 @@
 package org.pursuit.dowsingrod;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.login_button);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DowsingActivity.class));
+            }
+        });
     }
 }
